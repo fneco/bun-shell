292c8df7be610d9243a8b05dffb258e8f8737eb4
fneco <izumi.f.tech@gmail.com>
2024-04-18 23:33:32 +0900

    update config

--- auto-edit-clipboard/config.ts
+++ auto-edit-clipboard/config.ts
@@ -1,6 +1,12 @@
+import { piped } from "remeda";
 import type { Config } from "./type";
+
+const joinLines = (str: string) =>
+  str.replace(/([\w|\.|\,])(\r\n|\n|\r)([\w])/gm, "$1 $3");
+const deleteNewLine = (str: string) => str.replace(/(\r\n|\n|\r)/gm, "");
+const splitJPSentence = (str: string) => str.replace(/(。)/gm, "。\n");
 
 // example
 export const config: Config = {
-  onCopy: (str: string) => str.replace(/(\r\n|\n|\r)/gm, ""),
+  onCopy: piped(joinLines, deleteNewLine, splitJPSentence),
 };
