e8207a937b5f5d35c676a6a8a0d616cfcfb0bf59
fneco <izumi.f.tech@gmail.com>
2024-04-19 21:16:57 +0900

    update config

--- auto-edit-clipboard/config.ts
+++ auto-edit-clipboard/config.ts
@@ -4,9 +4,16 @@ import type { Config } from "./type";
 const joinLines = (str: string) =>
   str.replace(/([\w|\.|\,])(\r\n|\n|\r)([\w])/gm, "$1 $3");
 const deleteNewLine = (str: string) => str.replace(/(\r\n|\n|\r)/gm, "");
-const splitJPSentence = (str: string) => str.replace(/(。)/gm, "。\n");
+const deleteComment = (str: string) => str.replace(/\/\//gm, "");
+const breakLineWithAPunctuationMark = (str: string) =>
+  str.replace(/(。)/gm, "。\n");
 
 // example
 export const config: Config = {
-  onCopy: piped(joinLines, deleteNewLine, splitJPSentence),
+  onCopy: piped(
+    joinLines,
+    deleteNewLine,
+    deleteComment,
+    breakLineWithAPunctuationMark
+  ),
 };
