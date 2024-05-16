81563b2ddce397bdc0da954a7c2e320eabe04df4
fneco <izumi.f.tech@gmail.com>
2024-05-16 10:07:32 +0900

    build.ts
    bun build --watch > ~.js がうまく動作しなかったので変更

---
+++ auto-edit-clipboard/scripts/buildConfig.ts
@@ -0,0 +1,37 @@
+import { watch as bunWatch } from "fs/promises";
+import { parseArgs } from "util";
+
+const entrypoint = "./config.ts";
+
+const {
+  values: { watch },
+} = parseArgs({
+  strict: true,
+  allowPositionals: true,
+  options: { watch: { type: "boolean" } },
+});
+
+async function build() {
+  const output = await Bun.build({ entrypoints: [entrypoint] });
+
+  if (!output.success) {
+    console.log("build failed");
+    process.exit();
+  }
+  if (output.outputs.length !== 1) {
+    console.log("unexpected outputs");
+    process.exit();
+  }
+  const buildArtifact = output.outputs[0];
+  await Bun.write(buildArtifact.path, buildArtifact);
+}
+await build();
+
+if (watch) {
+  const watcher = bunWatch(process.cwd());
+  console.log(`start watching: ${entrypoint}`);
+  for await (const event of watcher) {
+    console.log(`Detected ${event.eventType} in ${event.filename}`);
+    await build();
+  }
+}
