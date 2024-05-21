188cb8da9e4781a73690832ab9b61c0458a62999
fneco <izumi.f.tech@gmail.com>
2024-05-21 13:00:03 +0900

    feat: remove unnecessary exit

--- auto-edit-clipboard/scripts/buildConfig.ts
+++ auto-edit-clipboard/scripts/buildConfig.ts
@@ -18,11 +18,9 @@ const build = oneAtATime(async function build() {
 
   if (!output.success) {
     console.log("build failed");
-    process.exit();
   }
   if (output.outputs.length !== 1) {
     console.log("unexpected outputs");
-    process.exit();
   }
   const buildArtifact = output.outputs[0];
   await Bun.write(buildArtifact.path, buildArtifact);
